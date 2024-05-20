d49910f3b96e96dfecc092eb8b255c99798e8a8c
Craig Walker <github@softcraft.ca>
2024-05-20 12:19:25 -0600

    insist

---
+++ src/nullish/insist.ts
@@ -0,0 +1,12 @@
+/**
+ * Ensures that the given value is not null or undefined.
+ * If the value is null or undefined, an error is thrown.
+ * @param value - The value to be checked.
+ * @returns The non-null non-undefined value.
+ * @throws {Error} If the value is null or undefined.
+ */
+export function insist<T>(value: T | null | undefined): T {
+  if (value === null) throw new Error("Value must not be null")
+  if (value === undefined) throw new Error("Value must not be undefined")
+  return value
+}
