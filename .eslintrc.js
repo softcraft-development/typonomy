d7fd7db5fb3089225af9ef866617a61997900a8d
Craig Walker <github@softcraft.ca>
2024-05-20 11:58:48 -0600

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
+  root: true,
+  rules: {
+    "func-names": ["error", "always"],
+    "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+    "no-console": "warn",
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "quote-props": ["error", "as-needed"],
+    quotes: ["error", "double"],
+    "sort-keys": ["error", "asc", { caseSensitive: true, natural: false }]
+  }
+}
