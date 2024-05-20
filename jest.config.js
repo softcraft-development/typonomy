febc72517a618d8dde368f57e482a211864ed080
Craig Walker <github@softcraft.ca>
2024-05-20 12:29:08 -0600

    absolute paths

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^nullish/(.*)": "<rootDir>/src/nullish/$1"
+  },
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
-  },
+  }
 }
