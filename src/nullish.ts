267cd7627df7129149fbced7655a13ee81833e20
Craig Walker <github@softcraft.ca>
2024-05-26 20:30:34 -0600

    utility types for nullish

--- src/nullish.ts
+++ src/nullish.ts
@@ -1,4 +1,45 @@
 /**
+ * A utility type for values that may never be undefined.
+ * @template T - The type when it is not undefined.
+ */
+export type Defined<T> = Exclude<T, undefined>
+
+/**
+* A utility type for values that may never be null nor undefined.
+* @template T - The type when it is not undefined.
+*/
+export type Definite<T> = Exclude<T, Nullish>
+
+/**
+ * A utility type for values that may be undefined.
+ * @template T - The type when it is not undefined.
+ */
+export type Optional<T> = T | undefined
+
+/**
+ * A utility type for values that may not be null.
+ * @template T - The type when it is not null.
+ */
+export type NotNullable<T> = Exclude<T, null>
+
+/**
+ * A utility type for values that may be null.
+ * @template T - The type when it is not null.
+ */
+export type Nullable<T> = T | null
+
+/**
+ * Either null or undefined.
+ */
+export type Nullish = null | undefined
+
+/**
+ * A utility type for values that may null or undefined.
+ * @template T - The type when it is not Nullish.
+ */
+export type Possible<T> = T | Nullish
+
+/**
  * Ensures that the given value is not null or undefined.
  * If the value is null or undefined, an error is thrown.
  * @param value - The value to be checked.
