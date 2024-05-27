2dbf112d2cc8be4596ed7fa1b639c58d0e2a5732
Craig Walker <github@softcraft.ca>
2024-05-26 20:02:29 -0600

    remove old name mapper

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  moduleNameMapper: {
-    "^nullish/(.*)": "<rootDir>/src/nullish/$1",
-  },
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
