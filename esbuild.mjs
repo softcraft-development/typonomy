bdbbd22429bfcd4e0ac10b4396eace782efcdf4a
Craig Walker <github@softcraft.ca>
2024-05-30 22:10:48 -0600

    build esm

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,8 +3,10 @@ import * as esbuild from "esbuild"
 esbuild.build({
   bundle: true,
   entryPoints: ["./src/**/*.ts"],
-  minify: true,
+  format: "esm",
+  // minify: true,
   outdir: "./dist",
   platform: "node",
   sourcemap: true,
+  target: "node14",
 }).catch(() => process.exit(1))
